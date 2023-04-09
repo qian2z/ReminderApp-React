55209e228c83f9aad640b90dd5f48f3284843c00
qian2z <98630339+qian2z@users.noreply.github.com>
2023-04-10 01:30:55 +0800

    handling form submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/reminder";
-
-const reminders: Reminder[] = [
-  { id: 1, title: "Reminder 1" },
-  { id: 2, title: "Reminder 2" },
-];
+import reminderService from "./services/reminder";
+import NewReminder from "./components/NewReminder";
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders();
+    setReminders(reminders);
+  };
+
+  const removeReminder = (id: number) => {
+    setReminders(reminders.filter((reminder) => reminder.id !== id));
+  };
+
+  const addReminder = async (title: string) => {
+    const newReminder = await reminderService.addReminder(title);
+    setReminders([newReminder, ...reminders]);
+  };
+
   return (
     <div className="App">
-      <ReminderList items={reminders}></ReminderList>
+      <NewReminder onAddReminder={addReminder}></NewReminder>
+      <ReminderList
+        items={reminders}
+        onRemoveReminder={removeReminder}
+      ></ReminderList>
     </div>
   );
 }
